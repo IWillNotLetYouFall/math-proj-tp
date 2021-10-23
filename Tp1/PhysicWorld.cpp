e7fc5e7068bca531fa94d6d21341ee0fbb2feede
Ouellet <gabriel262626@hotmail.com>
2021-10-22 21:09:37 -0400

    Added empty class as refs

--- Tp1/PhysicWorld.cpp
+++ Tp1/PhysicWorld.cpp
@@ -2,5 +2,15 @@
 
 void PhysicWorld::RunPhysics(float duration)
 {
-	//TODO: FORCES
+	//TODO: Générer forces sur les particules
+
+
+	//TODO: Effectuer la tâche d’intégration sur chacune des particules
+
+
+	//TODO: Détecter et faire la gestion des collisions
+	//Génération des Contacts
+
+	//Résolution des contacts
+
 }
